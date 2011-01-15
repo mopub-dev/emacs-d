ccf1bfc81ccf26b8767659be76b17663677e2a6d
John Pena <john.pena@gmail.com>
2011-01-15 14:09:49 -0800

    added elpa and some packages with it, including slime

--- init.el
+++ init.el
@@ -188,7 +188,22 @@
 (setq tags-file-name "~/TAGS")
 (global-set-key (kbd "<f5>") 'find-tag)
 
+
+(setq swank-clojure-binary "clojure")
+(require 'clojure-auto)
+(require 'swank-clojure-autoload)
+
+
 (require 'functions) ;; personal defuns
 (require 'mappings)  ;; personal key mappings
 
 ;; init.el ends here
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
